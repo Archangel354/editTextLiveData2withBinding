3673bc9e0761a9b6e5c07363817d8bdc9664aefb
Archangel354 <rduncan354@gmail.com>
2021-08-14 11:30:26 -0400

    added navController and findNavController to the MainActivity.kt by adding implementation "androidx.navigation:navigation-fragment-ktx:2.3.5"     implementation"androidx.navigation:navigation-ui-ktx:2.3.5" to build.gradle

--- app/src/main/java/com/example/android/edittextlivedata2withbinding/MainActivity.kt
+++ app/src/main/java/com/example/android/edittextlivedata2withbinding/MainActivity.kt
@@ -6,19 +6,30 @@ import androidx.appcompat.app.ActionBar
 import androidx.databinding.DataBindingUtil
 import com.example.android.edittextlivedata2withbinding.databinding.ActivityMainBinding
 import timber.log.Timber
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val topLevelDestinations = mutableSetOf<Int>()
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        Timber.plant(Timber.DebugTree())
+        topLevelDestinations.add(R.id.fragmentA)
+        topLevelDestinations.add(R.id.fragmentB)
+
+
 
 
         Timber.i("actionbar is null")
         val actionBar: ActionBar? = this.supportActionBar
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
-            actionBar.setTitle("I hope this works")
+            actionBar.setTitle("Main Activity")
         }
         else{
             Timber.i("actionbar is null")
